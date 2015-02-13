9580a2bf5ff13b3188f4e015aa870cbf4419ab55
Ivan Reese <ivan@cdiginc.com>
2015-02-12 18:22:51 -0700

    Add missing cd-foundation.js import

--- dist/libs.js
+++ dist/libs.js
@@ -2,4 +2,5 @@
 // @codekit-prepend "libs/modernizr.min.js";
 
 // 2. Sibling Dependencies
+// @codekit-prepend "../../cd-foundation/dist/cd-foundation.js";
 // @codekit-prepend "../../swfobject/swfobject/src/swfobject.js";
