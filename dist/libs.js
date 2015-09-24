1d8db2ffdc60bf24c2faa702cb045ff16f55665c
Ivan Reese <ivanreese@gmail.com>
2015-09-24 12:00:05 -0600

    Remove libs

--- dist/libs.js
+++
@@ -1,6 +0,0 @@
-// 1. Modernizr
-// @codekit-prepend "libs/modernizr.js";
-
-// 2. Sibling Dependencies
-// @codekit-prepend "../../cd-foundation/dist/cd-foundation.js";
-// @codekit-prepend "../../swfobject/swfobject/src/swfobject.js";
